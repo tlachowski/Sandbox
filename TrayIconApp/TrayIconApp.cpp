2a8692e15623f1687b2a70d5982ae9fcdb55c518
tlachowski <85612682+tlachowski@users.noreply.github.com>
2025-06-12 12:22:23 +0200

    Pierwszy commit: projekt C++ i workflow

---
+++ TrayIconApp/TrayIconApp.cpp
@@ -0,0 +1,76 @@
+﻿#include <windows.h>
+#include <shellapi.h>
+
+#define WM_TRAYICON (WM_USER + 1)
+#define ID_TRAY_EXIT 1001
+
+HINSTANCE hInst;
+NOTIFYICONDATA nid;
+
+// Function to handle messages
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_TRAYICON:
+        if (lParam == WM_RBUTTONDOWN) {
+            // Create a context menu
+            HMENU hMenu = CreatePopupMenu();
+            AppendMenu(hMenu, MF_STRING, ID_TRAY_EXIT, TEXT("Exit"));
+
+            // Display the menu
+            POINT pt;
+            GetCursorPos(&pt);
+            SetForegroundWindow(hwnd);
+            TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hwnd, NULL);
+            DestroyMenu(hMenu);
+        }
+        break;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == ID_TRAY_EXIT) {
+            Shell_NotifyIcon(NIM_DELETE, &nid); // Remove the tray icon
+            PostQuitMessage(0); // Exit the application
+        }
+        break;
+
+    case WM_DESTROY:
+        Shell_NotifyIcon(NIM_DELETE, &nid); // Clean up the tray icon
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    hInst = hInstance;
+
+    // Create a hidden window
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = TEXT("HiddenWindowClass");
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow(TEXT("HiddenWindowClass"), TEXT("Hidden Window"), 0, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
+
+    // Add a notification icon
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hwnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_TRAYICON;
+    nid.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Default application icon
+    lstrcpy(nid.szTip, TEXT("My Tray App"));
+    Shell_NotifyIcon(NIM_ADD, &nid);
+
+    // Message loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
